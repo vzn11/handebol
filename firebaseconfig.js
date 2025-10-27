76d8416cdc50e5af35892428ee4963c4b4b7ee1e
edertec <edertec@msn.com>
2025-10-27 14:22:36 -0300

    salvar no firebase

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,6 +1,3 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyB9ojTJWdx0MHdXVZqCu6BeuoJoWDCuQsE",
   authDomain: "handebol-94435.firebaseapp.com",
@@ -9,9 +6,8 @@ const firebaseConfig = {
   storageBucket: "handebol-94435.firebasestorage.app",
   messagingSenderId: "464336775363",
   appId: "1:464336775363:web:c09a223a473502ac071a76"
+
 };
 
-// Inicializa o Firebase
-const app = initializeApp(firebaseConfig);
-
-export { app };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
